b0cb000a3c67baa11bf52d26d85ec0c7b38076a8
dimacheverda <dima.cheverda@gmail.com>
2013-04-26 14:39:51 +0300

    Initial Commit

---
+++ Guess Who/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Guess Who
+//
+//  Created by Dima on 4/26/13.
+//  Copyright (c) 2013 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Guess Who/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Guess Who
+//
+//  Created by Dima on 4/26/13.
+//  Copyright (c) 2013 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
